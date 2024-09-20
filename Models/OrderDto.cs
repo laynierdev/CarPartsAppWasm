a5aa30e036c2bbcdcf7c57cac69f10f2c3623313
laynierdev <laynierdev@gmail.com>
2024-09-20 10:48:30 -0400

    Integrated Auth0 login in the app

---
+++ Models/OrderDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CarPartsAppWasm.Models;
+
+public class OrderDto
+{
+    public IList<ProductDto> Products { get; set; } = new List<ProductDto>();
+    public decimal Total { get; set; } = 0;
+    public DateTime Date { get; set; } = DateTime.Now;
+}
---
+++ Models/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CarPartsAppWasm.Models;
+
+public class ProductDto
+{
+ public string name { get; set; } = string.Empty;
+ public string description { get; set; } = string.Empty;
+ public decimal price { get; set; }
+ public int quantity { get; set; }
+ }
